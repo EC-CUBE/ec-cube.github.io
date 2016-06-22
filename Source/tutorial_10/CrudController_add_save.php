db567884b6ff4db87b16203bbe0a7475c29b3846
geany-y <1y.xxxsmokyxxx.y1@gmail.com>
2016-06-22 13:57:54 +0900

    tutorial10スニペット追記

---
+++ Source/tutorial_10/CrudController_add_save.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+namespace Eccube\Controller\Tutorial;
+
+use Eccube\Application;
+use Eccube\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class CrudController extends AbstractController
+{
+    public function index(Application $app, Request $request)
+    {
+        $app->clearMessage(); ★フラッシュメッセージをクリア
+
+        $Crud = new \Eccube\Entity\Crud();
+
+        $builder = $app['form.factory']->createBuilder('crud', $Crud);
+
+        $form = $builder->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $app->addSuccess('データーが保存されました'); ★メッセージをフラッシュメッセージに変更
+            $app['orm.em']->persist($Crud);
+            $app['orm.em']->flush($Crud);
+        } elseif($form->isSubmitted() && !$form->isValid()) {
+            $app->addError('入力内容をご確認ください'); ★メッセージをフラッシュメッセージに変更
+        }
+
+        $qb = $app['orm.em']->createQueryBuilder(); ★エンティティマネージャーからクエリビルダーを取得
+        $qb->select('cr') ★クエリ生成
+            ->from('\Eccube\Entity\Crud', 'cr')
+            ->orderBy('cr.update_date', 'desc');
+
+        $crudList = $qb->getQuery()->getResult(); ★取得結果:結果がなければ空配列が返却
+
+        return $app->render(
+            'Tutorial/crud_top.twig',
+            array(
+                'forms' => $form->createView(),
+                'crudList' => $crudList, ★データーベース取得値をビューに渡す
+            )
+        );
+    }
+}
